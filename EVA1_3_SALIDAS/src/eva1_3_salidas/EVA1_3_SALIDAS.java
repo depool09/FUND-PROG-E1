8fb85ea37824d8e7aeda875e70fc58be8c7c1219
depool09 <durandepool@gmail.com>
2026-09-03 18:48:32 -0600

    Add files via upload

---
+++ EVA1_3_SALIDAS/src/eva1_3_salidas/EVA1_3_SALIDAS.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_3_salidas;
+
+/**
+ *
+ * @author bisonte
+ */
+public class EVA1_3_SALIDAS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        System.out.println("Bienvenido a fundamentos de programacion");
+        System.out.println("Damian leobardo Duran Gonzalez");
+            System.out.print("tu calificacion sera :");
+            System.out.println(" 85");
+   
+    
+    }
+    
+}
---
+++ EVA_2_HOLA_MUNDO/src/eva_2_hola_mundo/EVA_2_HOLA_MUNDO.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva_2_hola_mundo;
+
+/**
+ *
+ * @author bisonte
+ */
+public class EVA_2_HOLA_MUNDO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {//INICIO DEL PROGRAMA 
+        // TODO code application logic here
+        //ESTOES UN BLOQUE DE CODIGO
+        //MOSTRAR UN MENSAJE DE TEXTO
+        //MOSTRAR--> INSTRUCCION
+        //TEXTO --> DATOS
+        System.out.println("HOLA MUNDO!!");
+       
+   //fin del programa
+    }
+}
